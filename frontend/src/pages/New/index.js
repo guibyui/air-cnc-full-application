071affa703f832fa4c1d88a8df86975ec2bb377f
guibyui <you@example.com>
2020-06-22 00:10:16 -0600

    changed thumbnail type

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,37 +1,36 @@
 import React, { useState, useMemo } from "react";
-import api from '../../services/api';
+import api from "../../services/api";
 
 import camera from "../../assets/camera.svg";
 
 import "./styles.css";
 
 export default function New({ history }) {
-  const [company, setCompany] = useState(null);
+  const [company, setCompany] = useState("");
   const [techs, setTechs] = useState("");
   const [price, setPrice] = useState("");
-  const [thumbnail, setThumbnail] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
 
   const preview = useMemo(() => {
     return thumbnail ? URL.createObjectURL(thumbnail) : null;
   }, [thumbnail]);
 
   async function handleSubmit(event) {
-
     event.preventDefault();
 
     const data = new FormData();
-    const user_id = localStorage.getItem('user');
+    const user_id = localStorage.getItem("user");
 
-    data.append('thumbnail', thumbnail);
+    data.append("thumbnail", thumbnail);
     data.append("company", company);
     data.append("techs", techs);
     data.append("price", price);
 
-    await api.post('/spots', data, {
-      headers: { user_id }
-    })
+    await api.post("/spots", data, {
+      headers: { user_id },
+    });
 
-    history.push('/dashboard');
+    history.push("/dashboard");
   }
 
   return (
